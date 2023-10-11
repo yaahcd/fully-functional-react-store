fe688a80fb6d078f3791b797250498a46c800a54
Yara <cremaschi.yara@gmail.com>
2023-10-11 10:07:56 +0100

    completely removed userContext from app

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { UserProvider } from "./contexts/UserContext.jsx";
 import { ProductsProvider } from "./contexts/ProductsContext.jsx";
 import { CartProvider } from "./contexts/CartContext.jsx";
 import { Provider } from "react-redux";
@@ -13,13 +12,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}> 
     <BrowserRouter>
-      {/* <UserProvider> */}
         <ProductsProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </ProductsProvider>
-      {/* </UserProvider> */}
     </BrowserRouter>
     </Provider>
   </React.StrictMode>
