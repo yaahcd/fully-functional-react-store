0fb1e57b98a77402077aa1aacd4c86aac92a796b
Yara <cremaschi.yara@gmail.com>
2023-10-11 10:53:06 +0100

    changed location for fetching items and completely removed products context

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,23 +1,8 @@
 import CategoryItem from "../categoryItem/CategoryItem";
 import categories from "./categories.json";
 import { CategoriesContainer } from "./Categories.styles";
-import { useDispatch } from "react-redux";
-import { setCategoriesMap } from "../../store/categories/category.action";
-import { useEffect } from "react";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 function Categories() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const getCategories = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
-    };
-
-    getCategories();
-  }, []);
-
   return (
     <CategoriesContainer>
       {categories.map(({ title, id, imageUrl, route }) => (
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,8 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categoriesPreview/CategoriesPreview";
 import CategoryPage from "../categoryPage/CategoryPage";
+import { useDispatch } from "react-redux";
+import { setCategoriesMap } from "../../store/categories/category.action";
+import { useEffect } from "react";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 function Shop() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+
+    getCategories();
+  }, []);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
