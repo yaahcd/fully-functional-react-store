f7e8a9a3183d80c0e2035a158905925f07826f02
Yara <cremaschi.yara@gmail.com>
2023-10-12 10:39:49 +0100

    added redux-persist to retain cart state

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { store, persistor } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
