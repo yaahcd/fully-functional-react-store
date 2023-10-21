9ed20e4bdba96266290c65ad3c7d2538d4212ee4
Yara <cremaschi.yara@gmail.com>
2023-10-21 12:08:50 +0100

    created spinner component and added styling

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.styles";
+
+function Spinner() {
+  return (
+    <SpinnerOverlay>
+      <SpinnerContainer></SpinnerContainer>
+    </SpinnerOverlay>
+  );
+}
+
+export default Spinner;
