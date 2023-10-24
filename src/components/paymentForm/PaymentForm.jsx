f44b90cfba895f62051cde6a21ab88947816f8f3
Yara <cremaschi.yara@gmail.com>
2023-10-24 12:51:33 +0100

    finished adding logic for payment form compnent and netlify serverless function

--- src/components/paymentForm/PaymentForm.jsx
+++ src/components/paymentForm/PaymentForm.jsx
@@ -6,26 +6,51 @@ import {
 } from "./paymentForm.styles";
 
 function PaymentForm() {
-    const stripe = useStripe();
-    const elements = useElements();
+  const stripe = useStripe();
+  const elements = useElements();
 
   const paymentHandler = async (e) => {
     e.preventDefault();
 
-    if(!stripe || !elements){
-        return;
+    if (!stripe || !elements) {
+      return;
     }
 
-    
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    const { client_secret } = response.paymentIntent;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Test user",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment successful");
+      }
+    }
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment</h2>
         <CardElement />
+        <PaymentButton>Pay now</PaymentButton>
       </FormContainer>
-      <PaymentButton>Pay now</PaymentButton>
     </PaymentFormContainer>
   );
 }
