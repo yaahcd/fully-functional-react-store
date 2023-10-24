0e44e6d6046ec97e254e52588c6c0e16f586c30e
Yara <cremaschi.yara@gmail.com>
2023-10-24 12:06:51 +0100

    added folder and first file to create netlify serverless function

--- src/components/paymentForm/PaymentForm.jsx
+++ src/components/paymentForm/PaymentForm.jsx
@@ -1,17 +1,33 @@
-import { CardElement } from "@stripe/react-stripe-js"
-import { PaymentFormContainer, FormContainer, PaymentButton } from "./paymentForm.styles"
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import {
+  PaymentFormContainer,
+  FormContainer,
+  PaymentButton,
+} from "./paymentForm.styles";
 
 function PaymentForm() {
+    const stripe = useStripe();
+    const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+
+    if(!stripe || !elements){
+        return;
+    }
+
+    
+  };
+
   return (
     <PaymentFormContainer>
-        <FormContainer>
-            <h2>Credit Card Payment</h2>
-            <CardElement/>
-        </FormContainer>
-        <PaymentButton>Pay now</PaymentButton>
-      
+      <FormContainer>
+        <h2>Credit Card Payment</h2>
+        <CardElement />
+      </FormContainer>
+      <PaymentButton>Pay now</PaymentButton>
     </PaymentFormContainer>
-  )
+  );
 }
 
-export default PaymentForm
+export default PaymentForm;
