c9f0cd7541796e526c56f4c9b64f52a3a1cb2a46
Yara <cremaschi.yara@gmail.com>
2023-10-26 14:21:04 +0100

    converted cart.types to TS

--- src/store/cart/cart.types.js
+++
@@ -1,6 +0,0 @@
-export const cartActionTypes = {
-    setCartItems: "cart/setCartItems",
-    setCartOpen: "cart/setCartOpen",
-    setCartCount: "cart/setCartCount",
-    setCartTotal: "cart/setCartTotal"
-  };
--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -19,11 +19,6 @@ export type FetchCategoriesFailed = ActionWithPayload<
   Error
 >;
 
-export type CategoryAction =
-  | FetchCategoriesStart
-  | FetchCategoriesSuccess
-  | FetchCategoriesFailed;
-
 export const fetchCategoriesStart = withMatcher((): FetchCategoriesStart =>
   createAction(categoriesActionTypes.fetchCategoriesStart));
 
