3a5870805b660b7ef951655350773a39db4305b8
Yara <cremaschi.yara@gmail.com>
2023-10-27 16:02:33 +0100

    converted user.types to TS

---
+++ src/store/user/user.types.ts
@@ -0,0 +1,15 @@
+export enum userActionTypes {
+    setCurrentUser = "user/setCurrentUser",
+    checkUserSession = "user/checkUserSession",
+    googleSignInStart = "user/googleSignInStart",
+    emailSignInStart = "user/emailSignInStart",
+    signInSuccess = "user/signInSuccess",
+    signInFailed = "user/signInFailed",
+    signUpStart = "user/signUpStart",
+    signUpSuccess = "user/signUpSuccess",
+    signUpFailed = "user/signUpFailed",
+    signOutStart = "user/signOutStart",
+    signOutSuccess = "user/signOutSuccess",
+    signOutFailed = "user/signOutFailed"
+  };
+  
