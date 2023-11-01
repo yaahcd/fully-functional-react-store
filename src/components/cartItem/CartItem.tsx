1cadb69d8aae5c4f15d166fba9663f13b43abace
Yara <cremaschi.yara@gmail.com>
2023-11-01 10:23:07 +0000

    converted cart items component and style to TS

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -1,6 +1,12 @@
+import { FC } from 'react';
 import { CartItemContainer, ItemDetailsContainer } from "./CartItem.styles";
+import { CartItem as TCartItem } from "../../store/cart/cart.types";
 
-function CartItem({ cartItem }) {
+export type CartItemProps = {
+  cartItem: TCartItem
+}
+
+const CartItem: FC<CartItemProps> = ({ cartItem }) => {
   return (
     <CartItemContainer>
       <img src={cartItem.imageUrl} alt={`Picture for ${cartItem.name}`} />
